ada95717f8257a0cb9058e58dde632d1bc918d40
DavidPersico <34723505+DavidPersico@users.noreply.github.com>
2020-03-25 17:10:49 -0600

    bitcpy.c: Define bitcpy function
    Basic memcpy function that uses bits instead of bytes.
    Used bitwise operations when possible for speed.
    Remaining bits are copied with bitwise OR AND mask.

---
+++ bitcpy.c
@@ -0,0 +1,47 @@
+#include "bitcpy.h"
+
+void* bitcpy(void* dest, const void* source, size_t num_bits)
+{
+	if (num_bits == 0)
+	{
+		return dest;
+	}
+
+	/* Cast void ptr to char ptr as chars are a single byte. */
+	char* char_dest = (char*)dest;
+	const char* char_source = (const char*)source;
+
+	size_t rem_bits;
+	size_t byte_index;
+	byte_index = 0;
+
+	/* Avoid unnecessary byte copy when possible. */
+	if (num_bits > 7)
+	{
+		size_t num_bytes;
+		/* Use bit shifting to divide efficiently. */
+		num_bytes = num_bits >> 3;
+
+		for (; byte_index < num_bytes; byte_index++)
+		{
+			char_dest[byte_index] = char_source[byte_index];
+		}
+
+		/* Use bitwise & to modulo efficiently. */
+		rem_bits = num_bits & 0x7;
+	}
+	else
+	{
+		rem_bits = num_bits;
+	}
+	
+	if (rem_bits == 0)
+	{
+		return dest;
+	}
+
+	/* Create mask of size rem_bits and use it to copy appropriate source bits to destination. */
+	char_dest[byte_index] = char_dest[byte_index] | ((1 << (rem_bits)) - 1) & char_source[byte_index];
+
+	return dest;
+}
